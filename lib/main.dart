78f76be1aa4cbed1a3006a0879d4d0a2223fe3e9
manvip28 <159559667+manvip28@users.noreply.github.com>
2025-03-02 18:26:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'pages/home_page.dart';
 import 'pages/isl_to_language_page.dart';
 import 'pages/language_to_isl_page.dart';
 import 'pages/result_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'pages/video_player_screen.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized before async operations
+
+  await Supabase.initialize(
+    url: <SUPABASE_PROJECT_URL> , // Replace with your Supabase project URL
+    anonKey: <SUPABASE_ANON_KEY] , // Replace with your Supabase anon key
+  );
+
   runApp(ISLTranslatorApp());
 }
 
@@ -12,6 +22,7 @@ class ISLTranslatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // Corrected syntax
       title: 'ISL Translator',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
@@ -26,7 +37,7 @@ class ISLTranslatorApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => HomePage(),
-        '/isl-to-language': (context) => ISLToLanguagePage(),
+        '/isl-to-language': (context) => IslToLanguagePage(),
         '/language-to-isl': (context) => LanguageToISLPage(),
         '/result': (context) => ResultPage(),
       },
